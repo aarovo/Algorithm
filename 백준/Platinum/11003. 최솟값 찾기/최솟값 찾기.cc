9d29b44c67a3f5c1ee5f8acd727b08e969263050
arovo <kingarovo@gmail.com>
2024-04-02 15:21:13 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1436 ms, Memory: 43588 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <deque> //덱을 사용하기 위한 라이브러리 추가
+#include <deque> 
 using namespace std;
-typedef pair<int,int> Node; //Node 타입 선언
+typedef pair<int,int> Node;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -10,19 +10,18 @@ int main(){
     
     int N,L;
     cin >> N >> L;
-    deque<Node> mydeque; //덱 자료구조
+    deque<Node> mydeque;
 
     for(int i=0;i<N;i++){
         int now;
-        cin >> now; //현재 데이터 입력
-        while(mydeque.size()&&mydeque.back().first > now) //?
+        cin >> now;
+        while(mydeque.size() && mydeque.back().first > now)
         {
-            mydeque.pop_back(); //덱 가장 오른쪽 데이터 삭제
+            mydeque.pop_back();
         }
 
-        mydeque.push_back(Node(now,i)); //덱 가장 오른쪽에 데이터 입력
+        mydeque.push_back(Node(now,i));
 
-        //범위에서 벗어난 값을 덱에서 제거
         if(mydeque.front().second <= i-L){
             mydeque.pop_front();
         }
