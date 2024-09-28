08720b1ec0687f19ea5fbf97df8607b27341bf8a
arovo <kingarovo@gmail.com>
2024-09-28 13:43:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <stack>
@@ -6,24 +5,23 @@
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
-    int answer = 0;
     int height = board.size();
     int move_cnt = moves.size();
     stack<int> basket;
+    int answer = 0;
     
-    for(int i=0;i<move_cnt;i++){
+    for(int i=0;i<move_cnt; i++){
         int pos = moves[i]-1;
-        for(int j=0;j<height;j++){
+        for(int j=0;j<height; j++){
             if(board[j][pos]==0){
                 continue;
             }
-            
             int doll = board[j][pos];
             board[j][pos] = 0;
             
-            if(!basket.empty() && doll == basket.top()){
+            if(!basket.empty()&&basket.top()==doll){
                 basket.pop();
-                answer += 2;
+                answer+=2;
             }
             else{
                 basket.push(doll);
@@ -31,5 +29,6 @@ int solution(vector<vector<int>> board, vector<int> moves) {
             break;
         }
     }
+    
     return answer;
 }
